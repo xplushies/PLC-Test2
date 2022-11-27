f8f08781598c2bf7bb56184a4e64b28ba4bf77ef
xplushies <100805230+xplushies@users.noreply.github.com>
2022-11-26 23:56:46 -0500

    Update partH.py

--- partH.py
+++ partH.py
@@ -1,11 +1,20 @@
 [Sample 1]:
-
+Sum = 6 + 7 - 4
+when (4 < 5)
+  total = 4,
+else
+  total = 0,
 
 [Sample 2]:
-
+Age = 56,
+Sentence = 'Emily was 9 years old.',
 
 (With Errors):
 [Sample 3]:
+  @345 = 678,
+  Tommy = 47,
 
 
 [Sample 4]:
+  applepieisprettygood = 'Hello, I am having an existential moment',
+  the_flu_was = 'I literally balck out for day :(',
