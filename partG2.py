2556f49343ecf8762414cef75a8b546b7705e377
xplushies <100805230+xplushies@users.noreply.github.com>
2022-11-26 23:36:53 -0500

    Update partG2.py

---
+++ partG2.py
@@ -0,0 +1,25 @@
+# 1 with a at least 5 syntax errors based on the rules you defined
+# Detail each error and say why it doesn’t work
+
+# syntax error 1because here is not a complete <bool_Expr>
+when (x = )
+  y = 'i',
+else
+  # syntax error 2 because its missing a "," at the end
+  # syntax error 3 because it's missing a "'" 
+  b = '4
+ 
+# syntax error 4 because it's missing the parentheses
+during  5 == 7 
+  # sytnax error 5 because the a_Sen is not complete
+  apple = ,
+  
+when ( 5 ==5)
+  t = 5 * 4,
+  i = 6,
+else
+  t = 0,
+  
+  
+
+
